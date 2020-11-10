cc44774eda521c469db76d264a3eb460bfdcdb99
Xavier <Xavie332@gmail.com>
2020-11-10 13:30:26 -0600

    added .css .html and linked base.html to web app

--- Art_webite/Art_webite/urls.py
+++ Art_webite/Art_webite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    path('about/', include('store.urls')),
 ]
